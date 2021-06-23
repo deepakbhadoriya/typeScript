1a72a0bdba4fbc98f9407f9dcd8619a339152103
Deepak Bhadoriya <deepakkbhadoriya@gmail.com>
2021-06-23 09:41:24 +0530

    maps user and company class setup

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,1 +1,7 @@
-console.log('hello');
+import { User } from './User';
+import { Company } from './Company';
+
+const user = new User();
+const company = new Company();
+console.log(user);
+console.log(company);
